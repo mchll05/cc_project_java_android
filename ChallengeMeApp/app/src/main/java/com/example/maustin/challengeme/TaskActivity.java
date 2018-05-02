3d4c865a8ecdf5e37184e9ec65255c2136e63d86
Michelle Austin <mchllaustin@gmail.com>
2018-05-02 17:07:58 +0100

    complete button work links

--- ChallengeMeApp/app/src/main/java/com/example/maustin/challengeme/TaskActivity.java
+++ ChallengeMeApp/app/src/main/java/com/example/maustin/challengeme/TaskActivity.java
@@ -3,6 +3,8 @@ package com.example.maustin.challengeme;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.maustin.challengeme.categoriesandchallenges.Challenge;
@@ -11,6 +13,7 @@ import com.example.maustin.challengeme.tasks.TaskListAdapter;
 public class TaskActivity extends AppCompatActivity {
 
     Challenge challenge;
+    Button completeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +26,14 @@ public class TaskActivity extends AppCompatActivity {
 
         TaskListAdapter taskListAdapter = new TaskListAdapter(this, challenge.getTaskList());
 
+        completeButton = findViewById(R.id.completeB);
+
         ListView listView = (ListView) findViewById(R.id.taskListId);
         listView.setAdapter(taskListAdapter);
+    }
 
-
+    public void onCompletedButtonClick(View button) {
+        Intent intent = new Intent(this, MyChallengesActivity.class);
+        startActivity(intent);
     }
 }
